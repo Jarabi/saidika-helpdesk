b78bb76868c3bdc8080edc82cc999217334e7e6d
Alex Jarabi <jarabialex@gmail.com>
2024-03-06 23:39:06 +0300

    Added logic to determine between user or admin dashboard views

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,14 +1,36 @@
-/**
- * Components
- */
+import { useEffect, useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import UserView from '../views/UserView';
+import AdminView from '../views/AdminView';
+import AuthContext from '../context/AuthProvider';
 
 const Dashboard = () => {
-  return (
-    <div className='dashboard'>
-      <UserView />
-    </div>
-  );
-}
+  const navigate = useNavigate();
+  const { auth } = useContext(AuthContext);
+  const [view, setView] = useState('user');
 
-export default Dashboard
+  const renderView = () => {
+    switch (view) {
+      case 'user':
+        return <UserView />;
+      case 'admin':
+        return <AdminView />;
+      default:
+        return <UserView />;
+    }
+  };
+
+  useEffect(() => {
+    if (!auth) {
+      navigate('/login');
+    } else if (auth.role === '100') {
+      setView('user');
+    } else if (auth.role === '200') {
+      setView('admin');
+    }
+  }, [auth, navigate]);
+
+  return <div className='dashboard'>{renderView()}</div>;
+};
+
+export default Dashboard;
